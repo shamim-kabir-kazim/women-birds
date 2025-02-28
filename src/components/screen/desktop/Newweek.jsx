6dec6d67bd26afada0a58b13c32fc490e463ef5e
shamim-kabir-kazim <cabrio.hexen@gmail.com>
2025-02-28 12:05:38 +0600

    frontend

--- src/components/screen/desktop/Newweek.jsx
+++ src/components/screen/desktop/Newweek.jsx
@@ -1,50 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from './Carousel';
 import './Newweek.css'; // Import the CSS file
 import TextHed from './TextHed';
 
-
 const Newweek = () => {
-  const items = [
-    {
-      name: 'Item 1',
-      price: '$10',
-      image:
-        'https://i.postimg.cc/Lsc197Hg/b316df6f2ab68e16f8195ff01ccf9b1b.jpg',
-    },
-    {
-      name: 'Item 2',
-      price: '$20',
-      image:
-        'https://fps.cdnpk.net/images/ai/image-generator/gallery/magnific-cat.webp',
-    },
-    {
-      name: 'Item 3',
-      price: '$30',
-      image:
-        'https://i.postimg.cc/Lsc197Hg/b316df6f2ab68e16f8195ff01ccf9b1b.jpg',
-    },
-    {
-      name: 'Item 3',
-      price: '$30',
-      image:
-        'https://i.postimg.cc/Lsc197Hg/b316df6f2ab68e16f8195ff01ccf9b1b.jpg',
-    },
-    {
-      name: 'Item 3',
-      price: '$30',
-      image:
-        'https://i.postimg.cc/Lsc197Hg/b316df6f2ab68e16f8195ff01ccf9b1b.jpg',
-    },
-  
-  ];
+  const [newWeekItems, setNewWeekItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/newview'); // Fetching new week items
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch new week items');
+        }
+
+        const data = await response.json();
+        setNewWeekItems(data.map(item => ({
+          name: item.product_name,
+          price: item.price,
+          image: `http://localhost:3000${item.primary_img_url}`,
+        })));
+      } catch (error) {
+        console.error('Error fetching new week items:', error);
+      }
+    };
+
+    fetchItems();
+  }, []);
 
   return (
     <div className="newweek-container">
       <div className="newweek-content">
         <TextHed text={"New This Week"} />
         <div className="newweek-list">
-          <Carousel items={items} />
+          <Carousel items={newWeekItems} />
         </div>
       </div>
     </div>
