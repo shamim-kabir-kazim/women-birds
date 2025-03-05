412607de261aa2d101a5f410dbde8bc3fe557490
shamim-kabir-kazim <cabrio.hexen@gmail.com>
2025-03-05 14:17:16 +0600

    frontend

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Product from '../screen/desktop/Product';
 import './ProductPage.css';
 
 const ProductPage = () => {
   const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const productId = searchParams.get('id');
+  const navigate = useNavigate();
+  const [productId, setProductId] = useState(null);
+
+  useEffect(() => {
+    const extractProductId = () => {
+      const searchParams = new URLSearchParams(location.search);
+      const id = searchParams.get('id');
+      
+      return id && !isNaN(Number(id)) ? id : null;
+    };
+
+    const extractedProductId = extractProductId();
+
+    setProductId(extractedProductId);
+
+    if (!extractedProductId) {
+      navigate('/', { replace: true });
+    }
+  }, [location, navigate]);
+
+  if (!productId) {
+    return null;
+  }
 
   return (
     <div className="home-div" style={{ width: '100%', height: '100%' }}>
